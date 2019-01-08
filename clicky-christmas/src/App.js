f1278652a73ccd3f3a8d56a1555330a0b2f54183
Kona Tess <konatess@gmail.com>
2019-01-07 23:51:57 -0800

    Added images and modified names for ease of use.

--- clicky-christmas/src/App.js
+++ clicky-christmas/src/App.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function importAll(r) {
+//   let images = {};
+//   r.keys().forEach((item) => { 
+//     images[item.replace('./', '')] = r(item); 
+//   });
+//   return images;
+// }
+
+function importAll(r) {
+  let images = {};
+  let allkeys = r.keys().map((item) => { 
+    let key = item.replace('./', '');
+    images[key] = r(item); 
+    return key;
+  });
+  console.log(allkeys)
+  return images;
+}
+
+const images = importAll(require.context('./images', false, /\.(png)$/));
+
+console.log(images);
+// console.log(images["santaclaus.png"]);
+
 class App extends Component {
   render() {
     return (
